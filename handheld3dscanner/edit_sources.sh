e8327387b0acc3983038ac7d1b8a58ae31001d6c
Frank Zhao <frank.zhao.main@gmail.com>
2019-09-30 21:19:56 -0700

    more comments

--- handheld3dscanner/edit_sources.sh
+++ handheld3dscanner/edit_sources.sh
@@ -1,7 +1,9 @@
 #!/bin/bash -e
 
+# this will enable additional dependancy sources, update the apt-get cache, and then upgrade all already-installed packages
+
 if ! grep -q 'enabled non free by script' /etc/apt/sources.list ; then
 	sudo sh -c 'echo "deb-src http://raspbian.raspberrypi.org/raspbian/ buster main contrib non-free rpi" >> /etc/apt/sources.list'
 	sudo sh -c 'echo "# enabled non free by script" >> /etc/apt/sources.list'
-	sudo apt-get update && sudo apt-get upgrade
+	sudo apt-get update && sudo apt-get upgrade -y
 fi
