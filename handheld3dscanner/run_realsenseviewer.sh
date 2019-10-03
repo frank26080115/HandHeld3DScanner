ff3999c37937f9728e7087897ccb764c10b482f8
Frank Zhao <frank.zhao.main@gmail.com>
2019-10-02 19:57:46 -0700

    automatically set recording file path

--- handheld3dscanner/run_realsenseviewer.sh
+++ handheld3dscanner/run_realsenseviewer.sh
@@ -1,4 +1,26 @@
 #!/bin/bash
 
 #realsense-viewer
-rs-pointcloud
+# this app takes up too much screen space
+# we have an alternative
+
+save_dir=~/pointcloud_saves
+mkdir -p $save_dir
+
+# check if an external disk is attached
+ext_disk=$(ls /media/pi | head -n1)
+if [ ! -z "$ext_disk" ]; then
+	tmp_dir=/media/pi/$ext_disk
+	if [ -d "$tmp_dir" ]; then
+		save_dir="/media/pi/$ext_disk/pointcloud_saves"
+		mkdir -p "$save_dir"
+	fi
+fi
+# note: if you are having trouble with attaching exFAT drives, try running
+# sudo apt-get install -y exfat-fuse exfat-utils
+# and also try to format the drive with a name that doesn't contain spaces
+
+save_path="$save_dir/pointcloud-$(date +%Y%m%d-%H%M%S).bag"
+
+rs-pointcloud "$save_path"
+# we have modifications to the rs-pointcloud app that allows it to save while running
