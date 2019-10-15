9dc28e354fb7ac5be8fd74228b35a9736d951473
Frank Zhao <frank.zhao.main@gmail.com>
2019-10-15 15:46:39 -0700

    prepare for rtabmap extra components

--- handheld3dscanner/build_ros_rtabmap.sh
+++ handheld3dscanner/build_ros_rtabmap.sh
@@ -61,27 +61,6 @@ if [ -f my.rosinstall ] ; then
 fi
 # note: for this script, we are not using the automatic installation generator so we are not using wstool
 
-# below are some optional parts of rtabmap
-# have not tested any of them, they may cause problems, check later
-#cd ${BASEDIR}
-#./build_giturl.sh https://github.com/borglab/gtsam 3.2.3
-#./build_giturl.sh https://github.com/willdzeng/cvsba
-#./build_giturl.sh https://github.com/RainerKuemmerle/g2o 20170730_git
-#./build_giturl.sh https://github.com/ethz-asl/libnabo 1.0.7
-#./build_giturl.sh https://github.com/ethz-asl/libpointmatcher 1.3.1
-#./build_giturl.sh https://github.com/OctoMap/octomap v1.7.1
-#./build_giturl.sh https://github.com/personalrobotics/OpenChisel
-#./build_giturl.sh https://github.com/fovis/fovis v1.1.0
-#./build_giturl.sh https://github.com/akhil22/libviso2
-#./build_giturl.sh https://github.com/tum-vision/dvo fuerte
-#./build_giturl.sh https://github.com/tum-vision/dvo_slam fuerte
-#./build_giturl.sh https://github.com/ethz-asl/okvis v1.1.1
-#./build_giturl.sh https://github.com/KumarRobotics/msckf_vio
-#./build_giturl.sh https://github.com/stevenlovegrove/Pangolin v0.5
-#./build_giturl.sh https://github.com/raulmur/ORB_SLAM2
-#./build_giturl.sh https://github.com/HKUST-Aerial-Robotics/VINS-Mono
-#./build_giturl.sh https://github.com/HKUST-Aerial-Robotics/VINS-Fusion
-
 #rosdep install --from-paths src -i -y -s --rosdistro kinetic --os=debian:buster --skip-keys=librealsense2 --skip-keys=rtabmap
 # rosdep would've tried to install libpcl 1.8 for some reason, but at this point, we have libpcl 1.9 already, and Buster doesn't seem to have 1.8 anyways
 sudo -H apt-get install -y libbullet-dev libsdl-image1.2-dev libsdl1.2-dev
@@ -97,7 +76,13 @@ if [ -f /opt/ros/kinetic/share/OpenCV-3.3.1-dev/OpenCVConfig.cmake ]; then
 	path_opencv_override='-DOpenCV_DIR=/opt/ros/kinetic/share/OpenCV-3.3.1-dev'
 fi
 
-extra_options='-DWITH_G2O=OFF -DWITH_GTSAM=OFF -DWITH_CVSBA=OFF'
+if [[ -z "$1" ]]; then
+	extra_options='-DWITH_G2O=OFF -DWITH_GTSAM=OFF -DWITH_CVSBA=OFF'
+	# removing these extra features is a safe build that's known to work
+	# use the $1 arg later to add extra features
+else
+	extra_options="-DWITH_G2O=OFF $1"
+fi
 
 # I've noticed some failures in catkin_make_isolated that might suggest we need to watch out for permission issues
 # the hack below is a nuclear solution
@@ -116,6 +101,14 @@ if [ -d ${rtabmap_catkin_ws}/devel_isolated ] ; then
 	sudo chmod -R ugo+rw ${rtabmap_catkin_ws}/devel_isolated
 fi
 
+if [ -f ${install_dir}/share/cpu_tsdf/CPUTSDFConfig.cmake ] ; then
+	export CPUTSDF_DIR=${install_dir}/share/cpu_tsdf
+fi
+
+if [ -f ~/extras_catkin_ws/src/ORB_SLAM2/lib/libORB_SLAM2.so ] ; then
+	export ORB_SLAM2_ROOT_DIR=$(readlink --canonicalize ~/extras_catkin_ws/src/ORB_SLAM2)
+fi
+
 cd ${rtabmap_catkin_ws}
 sudo rm make_outputlog.txt && true
 
@@ -125,7 +118,7 @@ n=0
 until [ $n -ge 10 ]
 do
 	catkin_failed=0
-	echo "calling catkin_make_isolated on $(date)"
+	echo -e "\e[32m calling catkin_make_isolated on $(date) \e[0m"
 	if sudo ./src/catkin/bin/catkin_make_isolated --install                                                \
 	                                              $path_opencv_override                                    \
 	                                              $extra_options                                           \
@@ -133,13 +126,12 @@ do
                                                   -DCMAKE_BUILD_TYPE=Release                               \
                                                   --install-space /opt/ros/kinetic                         \
                                                   -j4 2>&1 ; then
-		echo "catkin_make_isolated seems to have finished successfully"
+		echo -e "\e[32m catkin_make_isolated seems to have finished successfully \e[0m"
 	else
-		echo "catkin_make_isolated seems to have finished and has a failure"
+		echo -e "\e[31m catkin_make_isolated seems to have finished and has a failure \e[0m"
 		catkin_failed=1
 	fi
 	if [ $catkin_failed -ne 0 ] ; then
-		echo "catkin_make_isolated seems to have failed"
 		sudo chown -R $(id -u):$(id -g) ${install_dir}
 		sudo chown -R $(id -u):$(id -g) ${rtabmap_catkin_ws}/build_isolated
 		sudo chown -R $(id -u):$(id -g) ${rtabmap_catkin_ws}/devel_isolated
@@ -147,7 +139,7 @@ do
 		sudo chmod -R ugo+rw ${rtabmap_catkin_ws}/build_isolated
 		sudo chmod -R ugo+rw ${rtabmap_catkin_ws}/devel_isolated
 	else
-		echo "catkin_make_isolated seems to have succeeded"
+		echo -e "\e[32m catkin_make_isolated seems to have succeeded \e[0m"
 		break
 	fi
 	n=$[$n+1]
